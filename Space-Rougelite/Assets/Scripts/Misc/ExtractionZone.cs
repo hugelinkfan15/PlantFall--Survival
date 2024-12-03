0997f658a5d55d46d67a0edece4e1ee259996aec
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-12-03 13:27:04 -0600

    Extraction point working wtih indicator

--- Space-Rougelite/Assets/Scripts/Misc/ExtractionZone.cs
+++ Space-Rougelite/Assets/Scripts/Misc/ExtractionZone.cs
@@ -6,6 +6,8 @@ public class ExtractionZone : MonoBehaviour
 {
     //attach the parent object of the arrow attached to the player, not the arrow itself
     public GameObject playerArrow;
+    public GameObject playerTimeObj;
+    public DisplayBar playerTime;
 
     //NEED TO SET THE MIN AND MAX FOR EACH AXIS
     [SerializeField] private float minX;
@@ -14,10 +16,13 @@ public class ExtractionZone : MonoBehaviour
     [SerializeField] private float maxY;
 
     [SerializeField] private float countdown = 5.0f;
+    private float ogCD;
+    private bool start;
     // Start is called before the first frame update
     void Start()
     {
-       
+        ogCD = countdown;
+        playerTime.SetMaxValue(countdown);
     }
     /// <summary>
     /// Activate player to point towards point and moves point to a random location in bounds)
@@ -26,10 +31,20 @@ public class ExtractionZone : MonoBehaviour
     {
         transform.position = new Vector3(Random.Range(minX,maxX),Random.Range(minY,maxY),0);
         playerArrow.SetActive(true);
+        start = false;
     }
     // Update is called once per frame
     void Update()
     {
+        if(start)
+        {
+            playerTime.SetValue(countdown);
+            countdown -= Time.deltaTime;
+        }
+        if(countdown <= 0)
+        {
+            GameManager.Instance.gameOver = true;
+        }
     }
 
     /// <summary>
@@ -40,8 +55,11 @@ public class ExtractionZone : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(StartCountDown());
+            playerTimeObj.SetActive(true);
+            playerTime.SetMaxValue(countdown);
             playerArrow.SetActive(false);
+            playerTime.SetMaxValue(countdown);
+            start = true;
         }
     }
 
@@ -53,19 +71,11 @@ public class ExtractionZone : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            StopAllCoroutines();
+            start = false;
             playerArrow.SetActive(true);
-        }
-    }
-
-
-    IEnumerator StartCountDown()
-    {
-        while(true)
-        {
-            yield return new WaitForSeconds(countdown);
-
-            GameManager.Instance.gameOver = true;
+            countdown = ogCD;
+            playerTime.SetMaxValue(countdown);
+            playerTimeObj.SetActive(false);
         }
     }
 
