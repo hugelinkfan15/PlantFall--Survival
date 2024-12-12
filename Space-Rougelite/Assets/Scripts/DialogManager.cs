823d88657e51256d9162872a8006507465973fb3
Duncan Barner <duncan.barner@gmail.com>
2024-12-12 10:08:09 -0600

    DialogManager Mostly Working
    The dialog manager is working, button continues to the next sentence and properly displays tutorial
    **NEED TO FIX**
    Enemies still spawn, player can still move, time still increments while dialog is being displayed.

---
+++ Space-Rougelite/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement; // Import Scene Management
+
+public class DialogManager : MonoBehaviour
+{
+    public TMP_Text textbox;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    // Set references in the Inspector
+    public GameObject continueButton;
+    public GameObject dialogPanel;
+
+    // Optional: Specify the name of the next scene in the Inspector
+    public string nextSceneName;
+
+    void OnEnable()
+    {
+        index = 0; // Ensure index starts at 0 when the dialog is enabled
+        continueButton.SetActive(false);
+        StartCoroutine(Type());
+        Time.timeScale = 0;
+    }
+
+    IEnumerator Type()
+    {
+        textbox.text = "";
+        foreach (char letter in sentences[index])
+        {
+            textbox.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        continueButton.SetActive(true);
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textbox.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            // Final dialog reached
+            textbox.text = "";
+            dialogPanel.SetActive(false);
+            Time.timeScale = 1f;
+
+            // Load the next scene
+            //LoadNextScene();
+        }
+    }
+
+    /*void LoadNextScene()
+    {
+        // Option 1: Using the scene name specified in the Inspector
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            // Option 2: Load the next scene based on build index
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    } */
+}
